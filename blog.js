d68184e4fbad22461d7c9696059d49fb9c2bcfee
Vicky <Vicky@MacBook-Pro-de-Dalila.local>
2023-03-03 13:21:04 +0100

    boton cancelar editar

--- blog.js
+++ blog.js
@@ -18,7 +18,7 @@ function createPost(event){
     h3.innerText = textTitulo;
     p.innerText = textContenido;
 
-    iconoBorrar.classList.add("fa","fa-close");
+    iconoBorrar.classList.add("fa","fa-trash");
     iconoEditar.classList.add("fa","fa-pencil");
     
     article.appendChild(h3);
@@ -39,10 +39,40 @@ function deletePost(event){
     if(confirm("¿Deseas borrar este elemento? \n" + text)){
 
         parent.remove();
-
     }
+}
+
+function crearIconoEditar(){
+    let iconoEditar = document.createElement("i");
+    iconoEditar.classList.add("fa","fa-pencil");
+    iconoEditar.addEventListener("click",updatePost);
+    return iconoEditar;
+}
+
+function crearIcono(simnolo,callback){
+    let icono = document.createElement("i");
+    icono.classList.add("fa",simbolo);
+    icono.addEventListener("click",callback);
+    return icono;
 
 }
+
+
+
+function cancelEdit(event,textoTitulo,textoParrafo){
+    let element = event.target;
+    let parent = element.parentElement;
+    let titulo = document.createElement("h3");
+    let parrafo = document.createElement("p");
+    titulo.innerText = textoTitulo;
+    parrafo.innerText = textoParrafo;
+    parent.appendChild(titulo);
+    parent.appendChild(parrafo);
+    parent.getElementsByTagName("input")[0].remove();
+    parent.getElementsByTagName("texArea")[0].remove();
+
+}
+
 function updatePost(event){
     let element = event.target;
     let parent = element.parentElement;
@@ -52,43 +82,22 @@ function updatePost(event){
     let textArea = document.createElement("textArea");
     let br = document.createElement("br");
 
+
+    let iconoCancelar = document.createElement("i");
+    iconoCancelar.classList.add("fa","fa-close");
+
+    element.remove();
     inputTitulo.setAttribute("type","text");
     inputTitulo.value = titulo;
     textArea.value = texto;
     parent.appendChild(inputTitulo);
     parent.appendChild(br);
     parent.appendChild(textArea);
+    parent.appendChild(iconoCancelar);
 
-}
+    iconoCancelar.addEventListener("click",function(event){
+        cancelEdit(event,titulo,texto)
 
-
-
-
-
-/*function deletePost(event){
-    let element = event.target;
-    let parent = element.parentElement;
-    parent.remove();
-    let text = parent.getElementByTagName("h3").innerText;
-    if(confirm("¿Deseas borrar este elemento? \n" + text)){
-
-        parent.remove();
     }
-}*/
-
-
-/*
-let posts = [article];
-function ultimoPrimero(){
-    posts.sort(function(a, b){return b - a});
-    document.getElementsByTagName("article").innerHTML = posts;
+    );
 }
-
-
-
-
-    for(i = 0; i<article.lenght;i++){
-    }
-    document.getElementsByTagName("article").innerHTML = article;
-posts.reverse([i]);
-*/
